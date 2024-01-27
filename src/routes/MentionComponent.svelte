d6be86a0b07c750b9cfb0d50699141fa62401767
nichwch <nichwch@gmail.com>
2024-01-26 19:04:49 -0500

    cooler colors

--- src/routes/MentionComponent.svelte
+++ src/routes/MentionComponent.svelte
@@ -2,7 +2,7 @@
 	on:click={() => {
 		window.alert('hello ');
 	}}
-	class="bg-blue-200 hover:bg-blue-300 outline outline-black"
+	class="bg-blue-200 hover:bg-blue-300 outline outline-blue-700 text-blue-700"
 >
 	<slot />
 </button>
--- src/routes/ProjectComponent.svelte
+++ src/routes/ProjectComponent.svelte
@@ -2,7 +2,7 @@
 	on:click={() => {
 		window.alert('project');
 	}}
-	class="bg-green-200 hover:bg-green-300 outline outline-black"
+	class="bg-green-200 hover:bg-green-300 outline outline-green-700 text-green-700"
 >
 	<slot />
 </button>
