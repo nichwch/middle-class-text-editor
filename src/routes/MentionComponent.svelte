8ea7c0e416d3f8ed5ce936ded8e5e95d4503368a
nichwch <nichwch@gmail.com>
2024-01-26 22:48:38 -0500

    use border

--- src/routes/MentionComponent.svelte
+++ src/routes/MentionComponent.svelte
@@ -2,7 +2,7 @@
 	on:click={() => {
 		window.alert('hello');
 	}}
-	class="bg-blue-200 hover:bg-blue-300 outline outline-blue-700 text-blue-700"
+	class="bg-blue-200 hover:bg-blue-300 border-box border-2 border-blue-700 text-blue-700"
 >
 	<slot />
 </button>
--- src/routes/ProjectComponent.svelte
+++ src/routes/ProjectComponent.svelte
@@ -2,7 +2,7 @@
 	on:click={() => {
 		window.alert('project');
 	}}
-	class="bg-green-200 hover:bg-green-300 outline outline-green-700 text-green-700"
+	class="bg-green-200 hover:bg-green-300 border-box border-2 border-green-700 text-green-700"
 >
 	<slot />
 </button>
