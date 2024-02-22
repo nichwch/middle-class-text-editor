7c1ef94cc6e33cd02be625f7f7d387b37f36c5e7
nichwch <nichwch@gmail.com>
2024-02-22 18:27:23 -0500

    explanation of how it works

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Editor, splitText } from '$lib';
+	import { Editor } from '$lib';
 	import MentionComponent from './MentionComponent.svelte';
 	import ProjectComponent from './ProjectComponent.svelte';
 	let editorContents = `Good morning team! 
@@ -32,7 +32,7 @@ project/project/project
 	$: projects = getListFromString(projectInput);
 </script>
 
-<div class="w-full px-5 my-10 md:mx-auto md:w-[36rem]">
+<div class="w-full px-5 my-20 md:mx-auto md:w-[36rem]">
 	<h1 class="text-2xl mb-3">MIDDLE CLASS TEXT EDITOR</h1>
 	<h2 class="italic">"A rich text editor? In this economy?"</h2>
 	<h2><a class="link" href="https://nicholaschen.io">By Nicholas Chen</a></h2>
@@ -84,8 +84,19 @@ project/project/project
 	</p>
 	<p class="mt-3">From what I can tell, it works reasonably well on mobile.</p>
 	<h1 class="text-2xl mt-3">Some things that don't quite work:</h1>
-	<p class="mt-3">You can still get the styling to break by</p>
+	<p class="mt-3">
+		You can still get the styling to break by piling a bunch of keywords onto a line. This results
+		in a pretty localized styling failure that doesn't screw up the entire overlay, and it's also
+		not likely to happen with actual user input, so I'm fine with it.
+	</p>
+	<img
+		src="bug.png"
+		class="w-full border border-black mt-2"
+		alt="the bug where keywords pile up on a line"
+	/>
+	<p class="mt-3">Undo doesn't work after deleting a keyword. This is really annoying.</p>
 	<p class="mt-3"></p>
+
 	<h1 class="text-2xl mt-3">Use MIDDLE CLASS TEXT EDITOR in your project:</h1>
 	<p class="mt-3">
 		It's small enough that you can just copy paste the component into your project, <a
@@ -97,6 +108,34 @@ project/project/project
 	<p class="mt-3">
 		<a href="https://github.com/nichwch/middle-class-text-editor/blob/main/src/lib/Editor.svelte"
 			>You can find the source for the component here, in the repo.</a
+		>
+	</p>
+	<h1 class="text-2xl mt-3">How does it work?</h1>
+	<p class="mt-3">Great question! Short answer: By abusing absolute positioning.</p>
+	<p class="mt-3">
+		Long answer: I use three elements positioned absolutely so that they're all overlapping: an
+		"underlay div", the textarea, and an "overlay div".
+	</p>
+	<p class="mt-3">
+		The textarea is where the user types. As they type, the underlay clones the content one for one,
+		but renders keywords differently using a regex match.
+	</p>
+	<p class="mt-3">
+		The overlay renders all the keywords (again, using a regex match), and positions them absolutely
+		to match their corresponding keyword in the underlay. I use a separate overlay instead of
+		rendering keywords directly in the underlay because otherwise the z-index and stacking context
+		would prevent the keywords from being clickable.
+	</p>
+	<p class="mt-3">Here's a handy visualization of the setup.</p>
+	<img
+		src="explanation.jpg"
+		class="mt-2 w-full border border-black"
+		alt="An visualization of the underlay, textarea, overlay setup."
+	/>
+	<p class="mt-3">
+		If you got this far and liked my project, <a
+			href="https://github.com/nichwch/middle-class-text-editor"
+			>consider giving it a star on Github.</a
 		>
 	</p>
 </div>
